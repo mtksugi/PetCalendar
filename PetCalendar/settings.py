f5757dc487ad8632f7c16262c37ed682e7deacc9
mtk.sugi <63274801+mtksugi@users.noreply.github.com>
2021-12-25 13:40:29 +0900

    Update settings.py

--- PetCalendar/settings.py
+++ PetCalendar/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
@@ -129,15 +131,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-#　　↓↓↓heroku 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-# ↑↑↑heroku
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
@@ -150,3 +144,14 @@ LOGOUT_REDIRECT_URL = '/accounts/user_login'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+#　　for heroku
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+## Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+## Activate Django-Heroku.
+django_heroku.settings(locals())
+
