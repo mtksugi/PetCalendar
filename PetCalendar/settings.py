cc59ad7e4ef6d63edeb5f66b880547affabd42fc
mtk.sugi <63274801+mtksugi@users.noreply.github.com>
2022-02-28 20:53:34 +0900

    Update settings.py

--- PetCalendar/settings.py
+++ PetCalendar/settings.py
@@ -155,7 +155,7 @@ LOGOUT_REDIRECT_URL = '/accounts/user_login'
 
 # media settings
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = env('MEDIA_ROOT', default=os.path.join(BASE_DIR, 'media'))
 MEDIA_URL = '/media/'
 
 # email settings
